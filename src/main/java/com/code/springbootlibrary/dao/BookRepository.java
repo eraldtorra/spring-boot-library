b9fd0b47ae788b2890a66833ad6fdff9c1ded824
Ervin Torra <ervintorra@alienattorney.com>
2024-03-22 22:24:31 +0100

    Why:
    *
    
    This change addresses the need by:
    
    *

--- src/main/java/com/code/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/code/springbootlibrary/dao/BookRepository.java
@@ -1,9 +1,16 @@
 package com.code.springbootlibrary.dao;
 
 import com.code.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestResource
+
 public interface BookRepository extends JpaRepository<Book, Long>{
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
