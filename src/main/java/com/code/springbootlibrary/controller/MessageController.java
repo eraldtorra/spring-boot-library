0f210aa54b5d04e3624727baa2312ee98d237cc0
Ervin Torra <ervintorra@alienattorney.com>
2024-04-09 15:48:02 +0200

    Why:
    *
    
    This change addresses the need by:
    
    *

--- src/main/java/com/code/springbootlibrary/controller/MessageController.java
+++ src/main/java/com/code/springbootlibrary/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.code.springbootlibrary.controller;
 
 import com.code.springbootlibrary.entity.Message;
+import com.code.springbootlibrary.requestmodels.AdminQuestionRequest;
 import com.code.springbootlibrary.service.MessageService;
 import com.code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,18 @@ public class MessageController {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messageService.postMessage(message, userEmail);
 
+    }
 
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+
+        if (!admin.equals("admin")){
+            throw new Exception("Unauthorized");
+        }
+
+        messageService.putMessage(adminQuestionRequest, userEmail);
     }
 }
